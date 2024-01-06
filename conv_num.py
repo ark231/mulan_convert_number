a076a34a696f1df173b43af52a9bbf9a9ab869c8
ark231 <arklp20909@gmail.com>
2024-01-06 23:31:41 +0900

    fix: 符号が消えるバグ、浮動小数点の限界

--- conv_num.py
+++ conv_num.py
@@ -3,6 +3,8 @@ import sys
 
 import argparse
 
+from fractions import Fraction
+
 CONS = "KGSZTDNHMRPB"
 
 def num_to_int(num:str)->int:
@@ -48,29 +50,82 @@ def int_to_mudig(val:int)->str:
         tmp_value //= 12
     return result
 
+
+def num_to_fraction(num:str)->Fraction:
+    return Fraction(f"0.{num}")
+
+def mulan_to_fraction(mulan:str)->Fraction:
+    pass
+
+def mudig_to_fraction(mudig:str)->Fraction:
+    pass
+
+def fraction_to_num(val:Fraction,precision:int)->str:
+    return str(abs(val))[2:]
+
+def fraction_to_mulan(val:Fraction,precision:int)->str:
+    pass
+
+def fraction_to_mudig(val:Fraction,precision)->str:
+    assert val!=0.0
+    sign=val/abs(val)
+    abs_val=abs(val)
+    result=""
+    num_digs=0
+    while abs_val>0.0 and num_digs<precision:
+        dig=abs_val//Fraction(1,12)
+        result+=CONS[int(dig)]+(chr(0x0305)if sign<0 else "")+chr(0x0323)
+        num_digs+=1
+        abs_val-=dig*Fraction(1,12)
+        abs_val*=12
+    return result
+
+
 def main():
     parser=argparse.ArgumentParser()
     parser.add_argument("-f","--from",type=str,choices=["number","mulan","mulan-digits","mudig"],required=True)
     parser.add_argument("-t","--to",type=str,choices=["number","mulan","mulan-digits","mudig"],required=True)
-    parser.add_argument("-p","--precision",type=int,help="maximum number of digits in fraction part of mulan/mudig")
+    parser.add_argument("-p","--precision",type=int,default=12,help="maximum number of digits in fraction part of mulan/mudig")
     parser.add_argument("value",type=str)
     args=parser.parse_args()
     value=0
+    if "."in args.value:
+        int_part=args.value.split(".")[0]
+        frac_part=args.value.split(".")[1]
+    else:
+        int_part=args.value
+        frac_part=None
     match getattr(args,"from"):
         case "number":
-            value=num_to_int(args.value)
+            value=num_to_int(int_part)
+            value+=0 if frac_part is None else num_to_fraction(frac_part)
         case "mulan":
-            value=mulan_to_int(args.value)
+            value=mulan_to_int(int_part)
+            value+=0 if frac_part is None else mulan_to_fraction(frac_part)
         case "mulan-digits"|"mudig":
-            value=mudig_to_int(args.value)
+            value=mudig_to_int(int_part)
+            value+=0 if frac_part is None else mudig_to_fraction(frac_part)
     result=""
+    if value==0.0:
+        sign=1
+    else:
+        sign=int(value/abs(value))
+    if isinstance(value,Fraction):
+        frac_part=(abs(value)%1)
+        int_part=int(abs(value)-frac_part)
+        int_part*=sign
+        frac_part*=sign
+    else:
+        int_part=abs(value)
+        frac_part=None
+        int_part*=sign
     match args.to:
         case "number":
-            result=int_to_num(value)
+            result=f"{int_to_num(int_part)}{''if frac_part is None else f'.{fraction_to_num(frac_part,args.precision)}'}"
         case "mulan":
-            result=int_to_mulan(value)
+            result=f"{int_to_mulan(int_part)}{''if frac_part is None else f'{fraction_to_mulan(frac_part,args.precision)}'}"
         case "mulan-digits"|"mudig":
-            result=int_to_mudig(value)
+            result=f"{int_to_mudig(int_part)}{''if frac_part is None else f'{fraction_to_mudig(frac_part,args.precision)}'}"
     print(result)
 
 
